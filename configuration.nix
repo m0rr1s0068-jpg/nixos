119d74f59401b93a27ada16021ba97db9b2859a2
Ray Morris <ray@m0rr1s.com>
2026-03-27 12:33:25 -0400

    Adding nixos files

---
+++ configuration.nix
@@ -0,0 +1,59 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "iceman";
+
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "America/New_York";
+
+  users.users.ray = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEXquymX88l82mTrrAGge2sMTeYEresL26qWu2npHnvK ray@rogue"
+    ];
+    packages = with pkgs; [
+      tree
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    bat
+    btop
+    fastfetch
+    git
+    net-tools
+  ];
+
+  services.openssh = {
+    enable = true;
+    ports = [ 22 ];
+    settings = {
+      PasswordAuthentication = false;
+      AllowUsers = [ "ray" ];
+    UseDns = false;
+    X11Forwarding = false;
+    PermitRootLogin = "prohibit-password";
+    };
+  };
+
+  security.sudo = {
+    enable = true;
+    wheelNeedsPassword = false;
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  system.stateVersion = "25.11";
+}
+
